2e09b5fce29d96eddc8a0ff9cd272169739bb3f1
Andrea Tosato <andrea.tosato@hotmail.it>
2020-10-25 20:55:07 +0100

    Add Api Key

---
+++ DeviceControl/Authentications/ApiKeyOptions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace DeviceControl.Authentications
+{
+	public class ApiKeyOptions : AuthenticationSchemeOptions
+	{
+		public string KeyName { get; set; }
+
+		public string KeyValue { get; set; }
+	}
+}
