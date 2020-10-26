441d7fe178ef5b5dd48754df7add02d94c2ef32b
Marco Minerva <marco.minerva@gmail.com>
2020-10-26 10:37:54 +0100

    Create LedController

--- DeviceControl/Controllers/DeviceController.cs
+++ DeviceControl/Controllers/DeviceController.cs
@@ -9,12 +9,10 @@ namespace DeviceControl.Controllers
     public class DeviceController : ControllerBase
     {
         private readonly GpioService gpioService;
-        private readonly LedService ledService;
 
-        public DeviceController(GpioService gpioService, LedService ledService)
+        public DeviceController(GpioService gpioService)
         {
             this.gpioService = gpioService;
-            this.ledService = ledService;
         }
 
         [ProducesResponseType(StatusCodes.Status204NoContent)]
@@ -31,15 +29,6 @@ namespace DeviceControl.Controllers
         public async Task<IActionResult> PinCommand(PinCommand command)
         {
             await gpioService.SetPinAsync(command);
-            return NoContent();
-        }
-
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [HttpPost("led")]
-        [ProducesDefaultResponseType]
-        public async Task<IActionResult> LedCommand(LedCommand command)
-        {
-            await ledService.SetColorAsync(command);
             return NoContent();
         }
     }
---
+++ DeviceControl/Controllers/LedController.cs
@@ -0,0 +1,27 @@
+﻿using DeviceControl.Commands;
+using DeviceControl.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DeviceControl.Controllers
+{
+    public class LedController : ControllerBase
+    {
+        private readonly LedService ledService;
+
+        public LedController(LedService ledService)
+        {
+            this.ledService = ledService;
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpPost("")]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> SetLed(LedCommand command)
+        {
+            await ledService.SetColorAsync(command);
+            return NoContent();
+        }
+    }
+}
